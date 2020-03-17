0e61baf503a948530c7403d0139f18b5635dfc6c
Hiroshi Yamasaki <hiroshi_yamasaki@eastback.jp>
2020-03-17 11:05:42 +0900

    ADD 02-08

---
+++ 05/0501.rb
@@ -0,0 +1,9 @@
+# coding: utf-8
+#
+# 課題0501
+# 与えられた文字列を、単語ごとにアルファベット順に並べ替えて表示させて下さい。
+#  ※文字列を配列にするにはArray#splitメソッド、
+#   並べ替えにはArray#sortメソッドを使用すると便利です。
+#
+
+str = "watermelon,plum,pear,pineapple,banana,apple,apricot,strawberry,persimmon,kumquat,cherry,pomegranate,peach"
